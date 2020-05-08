193671ec1ed93303d86445d0f4d8f471fb7f8ea5
Oviom <matkovicsgabor@vipmail.hu>
2020-05-08 16:13:21 +0200

    changed tests to mock behaviour

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,48 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockStore1;
+  private TorpedoStore mockStore2;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockStore1 = mock(TorpedoStore.class);
+    mockStore2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockStore1, mockStore2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockStore1.isEmpty()).thenReturn(false);
+    when(mockStore1.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    //verify
+    verify(mockStore1, times(1)).isEmpty();
+    verify(mockStore1, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockStore1.fire(1)).thenReturn(true);
+    when(mockStore2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    
+    //verify
+    verify(mockStore1, times(1)).fire(1);
+    verify(mockStore2, times(1)).fire(1);
   }
 
 }
